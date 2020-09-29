03813cd00073b8b51bd9564ee9915791bea79efb
Kev8484 <kev8484@gmail.com>
2020-09-29 16:34:26 -0400

    run embedding.py

--- mimic_to_embed.sh
+++ mimic_to_embed.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-MIMIC_DIR="$HOME/mimic/data"
-mkdir -p "$MIMIC_DIR"
+MIMIC_DIR=$HOME/mimic/data
+mkdir -p $MIMIC_DIR
 # get mimic dataset from s3 bucket onto ec2 instance
 aws s3 cp "s3://mimic-deeplearning-text-cnn/mimic/data/*.csv $MIMIC_DIR"
+# preprec
+python preprocessing.py --mimic-dir $MIMIC_DIR
 
-python preprocessing.py --mimic-dir "$MIMIC_DIR"
-
-BERT_DIR="$HOME/bert/model"
+BERT_DIR=$HOME/bert/model
 mkdir -p "$BERT_DIR"
 # get bert models from s3 bucket onto ec2 instance
 aws s3 cp "s3://mimic-deeplearning-text-cnn/bert/model/* $BERT_DIR"
+python embedding.py --bert-dir $BERT_DIR --text-data --output-dir
 
