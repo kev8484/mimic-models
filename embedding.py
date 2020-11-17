8eaed61d62d667e9dd97d11e69ceda3993dbcc9f
Kev8484 <kev8484@gmail.com>
2020-11-17 10:11:54 -0500

    default behavior returns just the token tensors from BERT, option to return embedding tensors

--- embedding.py
+++ embedding.py
@@ -19,18 +19,6 @@ def load_bert():
     return tokenizer, model
 
 
-def fetch_raw_data(s3_bucket):
-    # create local directory to store fetched objects
-    local_data_dir = Path.cwd() / Path("data")
-    local_data_dir.mkdir(exist_ok=True)
-    # define target file names (s3 API requires string formatted paths)
-    local_raw_data = str(local_data_dir) + '/raw_text_data.csv'
-    # download object
-    s3_bucket.download_file(S3_RAW_TEXT, local_raw_data)
-
-    return local_raw_data
-
-
 def tokenize(tokenizer, text_file, batch_size=128, seq_length=128):
     token_li = []
     text_data = pd.read_csv(text_file)['TEXT']
@@ -50,6 +38,23 @@ def tokenize(tokenizer, text_file, batch_size=128, seq_length=128):
         token_li.append(tokens)
 
     return token_li
+
+
+def flatten_token_batches(tokens_li):
+    """ Convert list of dicts to tensors
+    """
+
+    input_ids = torch.cat([t.input_ids for t in tokens_li])
+    token_type_ids = torch.cat([t.token_type_ids for t in tokens_li])
+    attention_mask = torch.cat([t.attention_mask for t in tokens_li])
+
+    return input_ids, token_type_ids, attention_mask
+
+
+def format_filepath(output_dir, seq_len, file_type):
+    """ Helper function for formatting output filenames.
+    """
+    return f"{output_dir}/mimic_discharge_summaries_bert_{seq_len}tkns_{file_type}.pt"
 
 
 def move_to_GPU(input_ids, token_type_ids, attention_mask):
@@ -93,6 +98,8 @@ def parse_args(args):
                         help="File containing text data to tokenize/embed")
     parser.add_argument("--output-dir", type=Path, default=Path.cwd(),
                         help="The directory to save the output tensors")
+    parser.add_argument("-embeddings", action="store_true",
+                        help="Return word embedding tensors. Otherwise, return tokens")
     parser.add_argument("-b", "--batch-size", type=int, default=128,
                         help="Batch size")
     parser.add_argument("--seq-length", type=int, default=128,
@@ -112,10 +119,7 @@ def main(args):
     bert_tokenizer, model = load_bert()
     # fetch raw text csv from S3 or get from local path
     if args.text_data is None:
-        # fetch objects from s3 bucket
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(S3_BUCKET)
-        raw_data = fetch_raw_data(bucket)
+        raise RuntimeError("Must use --text-data to include a csv file with discharge notes")
     else:
         raw_data = args.text_data
     # tokenize text
@@ -126,20 +130,42 @@ def main(args):
         batch_size=args.batch_size,
         seq_length=args.seq_length,
     )
-    # generate embeddings
-    print("Embedding...")
-    embeddings = embed(model=model, token_batches=tokens)
-    # save to disk
-    print("Saving locally...")
-    outfile = str(args.output_dir) + \
-        f"/mimic_discharge_summaries_bert_{args.seq_length}tkns.pt"
-    torch.save(embeddings, outfile)
+
+    output_files = []
+
+    if not args.embeddings:
+        # save token tensors locally
+        input_ids, token_type_ids, attention_mask = flatten_token_batches(
+            tokens)
+        print("Saving locally...")
+        for tensor, file_type in zip([input_ids, token_type_ids, attention_mask],
+                                     ['input_ids', 'token_type_ids', 'attention_mask']):
+            outpath = format_filepath(
+                str(args.output_dir), args.seq_length, file_type)
+            torch.save(tensor, outpath)
+            output_files.append(outpath)
+
+    else:
+        # generate embeddings
+        print("Embedding...")
+        embeddings = embed(model=model, token_batches=tokens)
+        # save to disk
+        print("Saving locally...")
+        outpath = format_filepath(
+            str(args.output_dir), args.seq_length, "embeddings")
+        torch.save(embeddings, outpath)
+        output_files.append(outpath)
 
     if args.aws:
         print("Uploading to S3 bucket...")
         s3 = boto3.resource('s3')
-        s3.upload_file(
-            outfile, S3_BUCKET, f"data/mimic_discharge_summaries_pubmed_bert_{args.seq_length}tkns.pt")
+
+        for local_path in output_files:
+            # get just the file name from the path
+            filename = Path(local_path).name
+            # save to s3 bucket in directory called 'data'
+            s3.upload_file(local_path, S3_BUCKET, f"data/{filename}")
+
     print("Completed.")
 
 
